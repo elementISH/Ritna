07bc223d52b06abb30175d55146aa5c91aa77d74
unknown <ismail2mansour@gmail.com>
2022-03-06 15:41:33 +0200

    updated responsive and fixed js bugs

--- content/scripts/pages/form.js
+++ content/scripts/pages/form.js
@@ -1,5 +1,5 @@
 // form validation
-let subscribeSection = document.querySelector(".subscribe-form"),
+let contactSection = document.querySelector(".contact-form"),
   successfulSection = document.querySelector(".success-section");
 (submitButton = document.querySelector(".form-submit__btn")),
   (IDInput = document.querySelector(".id-input__container")),
@@ -12,9 +12,9 @@ let formData = [];
 let validInputsLength;
 let formLength;
 
-if (subscribeSection != null) {
-  let subscribeForm = document.querySelector(".subscribe-form__form-container");
-  let subscribeErrors = [
+if (contactSection != null) {
+  let contactForm = document.querySelector(".contact-form__form-container");
+  let contactErrors = [
     {
       nameRequired: document.querySelector(".name .required"),
       nameInvalid: document.querySelector(".name .normal"),
@@ -61,46 +61,46 @@ if (subscribeSection != null) {
     },
   ];
   async function validateForm() {
-    for (let i = 0; i < subscribeErrors.length; i++) {
-      let input = subscribeForm[i].getAttribute("name");
+    for (let i = 0; i < contactErrors.length; i++) {
+      let input = contactForm[i].getAttribute("name");
       await inputs.push(document.querySelector(`[name=${input}`));
     }
 
     // name input validation
 
     if (!inputs[0].value) {
-      subscribeErrors[0].nameRequired.style.display = "block";
-      subscribeErrors[0].nameInvalid.style.display = "none";
+      contactErrors[0].nameRequired.style.display = "block";
+      contactErrors[0].nameInvalid.style.display = "none";
       document.querySelector(".name .valid").style.display = "none";
-      subscribeErrors[0].isValid = false;
+      contactErrors[0].isValid = false;
     } else if (hasNumber(inputs[0].value)) {
-      subscribeErrors[0].nameRequired.style.display = "none";
-      subscribeErrors[0].nameInvalid.style.display = "block";
+      contactErrors[0].nameRequired.style.display = "none";
+      contactErrors[0].nameInvalid.style.display = "block";
       document.querySelector(".name .valid").style.display = "none";
-      subscribeErrors[0].isValid = false;
+      contactErrors[0].isValid = false;
     } else {
-      subscribeErrors[0].nameRequired.style.display = "none";
-      subscribeErrors[0].nameInvalid.style.display = "none";
-      subscribeErrors[0].isValid = true;
+      contactErrors[0].nameRequired.style.display = "none";
+      contactErrors[0].nameInvalid.style.display = "none";
+      contactErrors[0].isValid = true;
       document.querySelector(".name .valid").style.display = "block";
     }
 
     // email input validation
 
     if (!inputs[1].value) {
-      subscribeErrors[1].emailRequired.style.display = "block";
-      subscribeErrors[1].emailInvalid.style.display = "none";
+      contactErrors[1].emailRequired.style.display = "block";
+      contactErrors[1].emailInvalid.style.display = "none";
       document.querySelector(".email .valid").style.display = "none";
-      subscribeErrors[1].isValid = false;
+      contactErrors[1].isValid = false;
     } else if (!isEmail(inputs[1].value)) {
-      subscribeErrors[1].emailRequired.style.display = "none";
-      subscribeErrors[1].emailInvalid.style.display = "block";
+      contactErrors[1].emailRequired.style.display = "none";
+      contactErrors[1].emailInvalid.style.display = "block";
       document.querySelector(".email .valid").style.display = "none";
-      subscribeErrors[1].isValid = false;
+      contactErrors[1].isValid = false;
     } else {
-      subscribeErrors[1].emailRequired.style.display = "none";
-      subscribeErrors[1].emailInvalid.style.display = "none";
-      subscribeErrors[1].isValid = true;
+      contactErrors[1].emailRequired.style.display = "none";
+      contactErrors[1].emailInvalid.style.display = "none";
+      contactErrors[1].isValid = true;
       document.querySelector(".email .valid").style.display = "block";
     }
 
@@ -108,13 +108,13 @@ if (subscribeSection != null) {
 
     if (inputs[2].value) {
       if (!hasCharacter(inputs[2].value)) {
-        subscribeErrors[2].phoneInvalid.style.display = "block";
+        contactErrors[2].phoneInvalid.style.display = "block";
         document.querySelector(".phone .valid").style.display = "none";
-        subscribeErrors[2].isValid = false;
+        contactErrors[2].isValid = false;
       } else {
-        subscribeErrors[2].phoneInvalid.style.display = "none";
+        contactErrors[2].phoneInvalid.style.display = "none";
         document.querySelector(".phone .valid").style.display = "block";
-        subscribeErrors[2].isValid = true;
+        contactErrors[2].isValid = true;
       }
     }
 
@@ -122,12 +122,12 @@ if (subscribeSection != null) {
 
     if (inputs[3].value) {
       if (isMinimum(inputs[3].value)) {
-        subscribeErrors[3].noteInvalid.style.display = "block";
+        contactErrors[3].noteInvalid.style.display = "block";
         document.querySelector(".note .valid").style.display = "none";
-        subscribeErrors[3].isValid = false;
+        contactErrors[3].isValid = false;
       } else {
-        subscribeErrors[3].noteInvalid.style.display = "none";
-        subscribeErrors[3].isValid = true;
+        contactErrors[3].noteInvalid.style.display = "none";
+        contactErrors[3].isValid = true;
         document.querySelector(".note .valid").style.display = "block";
       }
     }
@@ -135,57 +135,57 @@ if (subscribeSection != null) {
     // age input validation
 
     if (!inputs[4].value) {
-      subscribeErrors[4].ageRequired.style.display = "block";
+      contactErrors[4].ageRequired.style.display = "block";
       document.querySelector(".age .valid").style.display = "none";
-      subscribeErrors[4].isValid = false;
+      contactErrors[4].isValid = false;
     } else {
-      subscribeErrors[4].ageRequired.style.display = "none";
+      contactErrors[4].ageRequired.style.display = "none";
       document.querySelector(".age .valid").style.display = "block";
-      subscribeErrors[4].isValid = true;
+      contactErrors[4].isValid = true;
     }
 
     // country input validation
 
     if (inputs[5].value) {
       document.querySelector(".country .valid").style.display = "block";
-      subscribeErrors[5].isValid = true;
+      contactErrors[5].isValid = true;
     }
 
     // id
 
     if (!inputs[6].value) {
-      subscribeErrors[6].IDRequired.style.display = "block";
-      subscribeErrors[6].IDInvalid.style.display = "none";
+      contactErrors[6].IDRequired.style.display = "block";
+      contactErrors[6].IDInvalid.style.display = "none";
       document.querySelector("#idValid").style.display = "none";
-      subscribeErrors[6].isValid = false;
+      contactErrors[6].isValid = false;
     } else if (!hasCharacter(inputs[6].value)) {
-      subscribeErrors[6].IDRequired.style.display = "none";
-      subscribeErrors[6].IDInvalid.style.display = "block";
+      contactErrors[6].IDRequired.style.display = "none";
+      contactErrors[6].IDInvalid.style.display = "block";
       document.querySelector("#idValid").style.display = "none";
-      subscribeErrors[6].isValid = false;
+      contactErrors[6].isValid = false;
     } else {
-      subscribeErrors[6].IDRequired.style.display = "none";
-      subscribeErrors[6].IDInvalid.style.display = "none";
-      subscribeErrors[6].isValid = true;
+      contactErrors[6].IDRequired.style.display = "none";
+      contactErrors[6].IDInvalid.style.display = "none";
+      contactErrors[6].isValid = true;
       document.querySelector("#idValid").style.display = "block";
     }
 
     // passport
 
     if (!inputs[7].value) {
-      subscribeErrors[7].passportRequired.style.display = "block";
-      subscribeErrors[7].passportInvalid.style.display = "none";
+      contactErrors[7].passportRequired.style.display = "block";
+      contactErrors[7].passportInvalid.style.display = "none";
       document.querySelector("#passportValid").style.display = "none";
-      subscribeErrors[7].isValid = false;
+      contactErrors[7].isValid = false;
     } else if (!hasCharacter(inputs[7].value)) {
-      subscribeErrors[7].passportRequired.style.display = "none";
-      subscribeErrors[7].passportInvalid.style.display = "block";
+      contactErrors[7].passportRequired.style.display = "none";
+      contactErrors[7].passportInvalid.style.display = "block";
       document.querySelector("#passportValid").style.display = "none";
-      subscribeErrors[7].isValid = false;
+      contactErrors[7].isValid = false;
     } else {
-      subscribeErrors[7].passportRequired.style.display = "none";
-      subscribeErrors[7].passportInvalid.style.display = "none";
-      subscribeErrors[7].isValid = true;
+      contactErrors[7].passportRequired.style.display = "none";
+      contactErrors[7].passportInvalid.style.display = "none";
+      contactErrors[7].isValid = true;
       document.querySelector("#passportValid").style.display = "block";
     }
 
@@ -198,14 +198,14 @@ if (subscribeSection != null) {
   // posting data if everything is valid
   async function validateAll() {
     validInputsLength = 1;
-    formLength = subscribeForm.length - 1;
-    for (let valid = 0; valid < subscribeErrors.length; valid++) {
-      if (subscribeErrors[valid].isValid === true) {
+    formLength = contactForm.length - 1;
+    for (let valid = 0; valid < contactErrors.length; valid++) {
+      if (contactErrors[valid].isValid === true) {
         validInputsLength++;
       }
     }
     if (validInputsLength == formLength) {
-      subscribeSection.style.display = "none";
+      contactSection.style.display = "none";
       successfulSection.style.display = "block";
       for (let data = 0; data < inputs.length; data++) {
         let dataValues = inputs[data].value;
